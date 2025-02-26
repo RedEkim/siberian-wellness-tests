0692a6dc552e16f210abb34133b74f4b29017c3c
redEkim <mikhail6886@yandex.ru>
2025-02-26 10:23:09 +0500

    added webConfig (local and remote web-launch)

---
+++ src/test/java/config/WebConfig.java
@@ -0,0 +1,31 @@
+package config;
+
+import org.aeonbits.owner.Config;
+@Config.Sources({
+        "classpath:${envWeb}.properties"
+})
+public interface WebConfig extends Config {
+
+    @Key("baseUrl")
+    @DefaultValue("https://countrystage.siberianhealth.com/kz-ru/")
+    String getBaseUrl();
+
+    @Key("baseUri")
+    @DefaultValue("https://countrystage.siberianhealth.com/kz-ru/")
+    String getBaseUri();
+
+    @Key("browserName")
+    @DefaultValue("chrome")
+    String getBrowserName();
+
+    @Key("browserVersion")
+    @DefaultValue("100")
+    String getBrowserVersion();
+
+    @Key("browserSize")
+    @DefaultValue("1920x1080")
+    String getBrowserSize();
+
+    @Key("remoteUrl")
+    String getRemoteUrl();
+}
