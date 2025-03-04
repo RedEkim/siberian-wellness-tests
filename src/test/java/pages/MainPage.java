7ea2bf164e041d5c9daac4b426147f4b941b8014
redEkim <mikhail6886@yandex.ru>
2025-03-04 22:07:30 +0500

    updated MainPage

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
+import net.nosma.Main;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
@@ -11,7 +12,10 @@ public class MainPage {
     private final SelenideElement
         toProfile = $("[data-qa='HEADER_PROFILE']"),
         loginRegistrationText = $("[data-qa='VLOGIN_REGISTRATION_BAR_DESCRIPTION']"),
-        toLogin = $("[data-qa='VLOGIN_REGISTRATION_BAR_LOGIN_LINK']");
+        toLogin = $("[data-qa='VLOGIN_REGISTRATION_BAR_LOGIN_LINK']"),
+        btnCart = $(".cart-button-img"),
+        btnMakeOrder = $("[data-qa='MAKE_ORDER']"),
+        toCart = $(".header__cart .cart-bar__icon");
 
     @Step("Open main page")
     public MainPage openPage(String value) {
@@ -37,5 +41,22 @@ public class MainPage {
         return this;
     }
 
+    @Step("add item to cart")
+    public MainPage addItemToCart() {
+        btnCart.click();
+        return this;
+    }
+
+    @Step("Open cart")
+    public MainPage clickByCart() {
+        toCart.click();
+        return this;
+    }
+
+    @Step("Make order")
+    public MainPage clickByMakeOrder() {
+        btnMakeOrder.click();
+        return this;
+    }
 
 }
