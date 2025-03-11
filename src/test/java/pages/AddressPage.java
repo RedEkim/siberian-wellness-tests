a50734eae72f618f2ed6b8c8a228320dee36152b
redEkim <mikhail6886@yandex.ru>
2025-03-11 21:16:52 +0500

    added new Page(AddressPage)

---
+++ src/test/java/pages/AddressPage.java
@@ -0,0 +1,21 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class AddressPage {
+    private final SelenideElement
+        btnAddAddressStandart = $("[data-qa='SW_BUTTON']"),
+        btnToolTip = $("[data-qa='SW_TOOLTIP_BUBBLE']"),
+        btnMakeMain = $("[data-qa='SW_MENU_ITEMS_TEXT']").find("Сделать основным"),
+        btnDeleteAddress = $("[data-qa='SW_MENU_ITEMS_TEXT']").find("Удалить"),
+        snackBarAboutDeleting = $("[data-qa='SW_SNACKBAR_HEADER']"),
+        noAddressTitle = $(".profile-delivery-address__no-addresses-title"),
+        noAddressDescription = $(".profile-delivery-address__no-address-description"),
+        btnAddAddressWithoutAddress = $("[data-qa='PROFILE_DELIVERY_ADDRESS_ADD_ADDRESS_BUTTON']"),
+        postIndex = $("input[class='sw-input__value sw-input__value_placeholder address-form-kz__index']"),
+        streetName = $("input[class='sw-input__value sw-input__value_placeholder address-form-kz__street']"),
+        house = $("input[class='sw-input__value sw-input__value_placeholder address-form-kz__house']"),
+        addAddress = $("SW_BUTTON").find("Добавить адрес");
+}
